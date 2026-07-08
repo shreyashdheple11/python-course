9a62d45463ced35eff1ad6a44d61ab19fa3db015
Shreyash <shreyashdheple.0588@gmail.com>
2026-07-08 19:56:53 +0530

    add loops file

---
+++ loops/code.py
@@ -0,0 +1,14 @@
+# # count = 0 
+# # while count < 20:
+# #     print('hi,  i am shreyash')
+# #     count = count + 2
+
+# x = 5
+# while x > 0:
+#     print(x)
+#     x = x - 1
+
+answer = " "
+while answer != "quit":
+    answer = input('Type somwthing:')
+    print("You Typed: ", answer)
