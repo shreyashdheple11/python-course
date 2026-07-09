c85070586b2697714a7999197761228ada59fa09
Shreyash <shreyashdheple.0588@gmail.com>
2026-07-09 13:40:21 +0530

    loops with else

--- loops/code.py
+++ loops/code.py
@@ -1,14 +1,28 @@
-# # count = 0 
-# # while count < 20:
-# #     print('hi,  i am shreyash')
-# #     count = count + 2
+# # # count = 0 
+# # # while count < 20:
+# # #     print('hi,  i am shreyash')
+# # #     count = count + 2
 
-# x = 5
-# while x > 0:
-#     print(x)
-#     x = x - 1
+# # x = 5
+# # while x > 0:
+# #     print(x)
+# #     x = x - 1
 
-answer = " "
-while answer != "quit":
-    answer = input('Type somwthing:')
-    print("You Typed: ", answer)
+# answer = " "
+# while answer != "quit":
+#     answer = input('Type somwthing:')
+#     print("You Typed: ", answer)
+# else:
+#     print("Program terminated Success !")
+
+sport_lst = ["Cricket", "Football", "Badminton", "Rugby", "Tennnis", "Volleyball",
+ "carrom", "chess", "running"]
+lst_len = len(sport_lst)
+index = 0
+while index < lst_len:
+    if sport_lst[index] == "Rugby":
+        print('Rugby is available')
+        break
+    index += 1
+else:
+    print('Rugby isnt available')
