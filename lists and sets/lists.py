1d3dfde01fc13a2406d582281f206aec7976cada
Shreyash Dheple <shreyashdheple.0588@gmail.com>
2026-07-12 22:31:09 +0530

    add items to list

--- lists and sets/lists.py
+++ lists and sets/lists.py
@@ -1,4 +1,4 @@
-groccery_items = ['handwash','bathsoap', 'cleaner', 't shirts', 'tea powder', 'sugar']
+groccery_items = ['handwash','bathsoap', 'cleaner', 't shirts', 'tea powder', 'sugar', 'coffee', 'milk', 'eggs', 'bread']
 print('No of items: ',len(groccery_items))
 groccery_items.insert(2,'detergent')
 print(groccery_items)
