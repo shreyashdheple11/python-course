e1890fbf280b1dd2c181bd6b238e2de34c101aaa
Shreyash Dheple <shreyashdheple.0588@gmail.com>
2026-07-13 22:09:24 +0530

    first and last item

--- lists and sets/lists.py
+++ lists and sets/lists.py
@@ -2,3 +2,6 @@ groccery_items = ['handwash','bathsoap', 'cleaner', 't shirts', 'tea powder', 's
 print('No of items: ',len(groccery_items))
 groccery_items.insert(2,'detergent')
 print(groccery_items)
+print('No of items: ',len(groccery_items))
+print('The first item in the list is: ',groccery_items[0])
+print('The last item in the list is: ',groccery_items[-1])
